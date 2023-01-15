68e387a4410814dfcf87036bae75e017662057c6
Ishrak2692 <95377811+Ishrak2692@users.noreply.github.com>
2023-01-15 12:48:44 +0600

    Add files via upload

---
+++ src/components/Notice.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import img1 from'./images/Fashion1.jpg';
+
+
+
+function Notice  () {
+  return (
+    <div>
+
+<img    src="http://cdn.shopify.com/s/files/1/0082/3944/8160/collections/Collection-Banner-winter-02_1200x1200.jpg?v=1608540700"/>
+    </div>
+    //<img  style={{height:1008}}   src={img1}/>
+  )
+}
+
+export default Notice
